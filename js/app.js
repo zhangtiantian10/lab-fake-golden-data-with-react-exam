fba5d1b45555ec633d97d2388647e88f0c9a86c9
zhangtiantian10 <975571814@qq.com>
2016-08-04 10:01:10 +0800

    add form

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ class View extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isEditor: true
+            isEditor: true,
+            elements: []
         };
     }
 
@@ -12,11 +13,20 @@ class View extends React.Component {
         });
     }
 
+    addElement(type) {
+        let elements = this.state.elements;
+
+        elements.push(type);
+        this.setState({
+            elements: elements
+        });
+    }
+
     render() {
         return <div>
             <button onClick={this.viewChange.bind(this)}>{this.state.isEditor ? "Preview" : "Edit"}</button>
             <div className={this.state.isEditor ? "" : "hidden"}>
-                <Editor />
+                <Editor onAdd={this.addElement.bind(this)} elements={this.state.elements}/>
             </div>
             <div className={this.state.isEditor ? "hidden" : ""}>
                 <Preview />
@@ -28,7 +38,36 @@ class View extends React.Component {
 class Editor extends React.Component {
     render() {
         return <div>
-            Editor
+            <RightButton onAdd={this.props.onAdd}/>
+            <LeftPanel elements={this.props.elements}/>
+        </div>;
+    }
+}
+
+class RightButton extends React.Component {
+    add(type) {
+        this.props.onAdd(type);
+    }
+
+    render() {
+        return <div>
+            <button onClick={this.add.bind(this, 'text')}>Text</button>
+            <button onClick={this.add.bind(this, 'date')}>Date</button>
+        </div>;
+    }
+}
+
+class LeftPanel extends React.Component {
+    render() {
+        const elements =this.props.elements.map((element, index) => {
+            return <div key={index}>
+                <input type={element}/>
+                <button>X</button>
+            </div>;
+        })
+
+        return <div>
+            {elements}
         </div>
     }
 }
