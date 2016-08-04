651e4f1a36eb0f0dcacd969dfca34bf9ac7937a4
zhangtiantian10 <975571814@qq.com>
2016-08-04 19:50:05 +0800

    view can change

--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
 class View extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditor: true
+        };
+    }
+
+    viewChange() {
+        this.setState({
+            isEditor: !this.state.isEditor
+        });
+    }
+
     render() {
         return <div>
-            Hello
+            <button onClick={this.viewChange.bind(this)}>{this.state.isEditor ? "Priview" : "Editor"}</button>
+            <div className={this.state.isEditor ? "" : "hidden"}><Editor /></div>
+            <div className={this.state.isEditor ? "hidden" : ""}><Preview /></div>
+        </div>;
+    }
+}
+
+class Editor extends React.Component {
+    render() {
+        return <div>
+            Editor
+        </div>
+    }
+}
+
+class Preview extends React.Component {
+    render() {
+        return <div>
+            Preview
         </div>
     }
 }
