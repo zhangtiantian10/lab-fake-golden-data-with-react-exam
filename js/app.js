ca223747b67657e942498b436e7ab2a9cec887d9
zhangtiantian10 <975571814@qq.com>
2016-08-04 09:47:08 +0800

    View Component is implement

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// TODO
+class View extends React.Component {
+    render() {
+        return <div>
+            Hello
+        </div>
+    }
+}
+
+ReactDOM.render(<View />, document.getElementById('content'));
